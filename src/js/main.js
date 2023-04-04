e241584dbf5311fc08ef42fde25b094d49ebdebb
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-04-04 10:46:51 +0100

    Created an if statement in js file for hamburger menu, event listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
-console.log(
-    "hello everyone!"
-);
+// Hamburger menu
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+
+// Adding if statement, if user click on hamburger menu 'right' pixels would change from -300px to 0px 
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
