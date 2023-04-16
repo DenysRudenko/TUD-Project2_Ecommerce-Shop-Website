82fe8d3e322ebfddd52922ca28a3df5190380c29
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-04-16 03:51:14 +0100

    Created two functions for counting clicking times and saving the info

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,50 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
+let carts = document.querySelectorAll('.add-cart');
+let products = [
+    {
+        name: 'Mens Fashion T-Shirt',
+        tag: 'fashiontshirt',
+        price: 15,
+        inCart: 0
+    }
+]
 
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers();
+    });
+}
+
+// Allows to save the number of times we clicked on the item behing cart
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if (productNumbers) {
+        document.querySelector('#navbar span').textContent = productNumbers;
+    }
+}
+
+// Allows us to count number of times we click on item that added to cart
+function cartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+
+    productNumbers = parseInt(productNumbers);
+    
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+         document.querySelector('#navbar span').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('#navbar span').textContent = 1;
+    }
+
+    
+}
+
+onLoadCartNumbers();
 
 // Adding if statement, if user click on hamburger menu 'right' pixels would change from -300px to 0px 
 
@@ -40,3 +83,4 @@ var MainImg = document.getElementById("MainImg");
         smallimg[3].onclick = function () {
             MainImg.src = smallimg[3].src;
         }
+        
